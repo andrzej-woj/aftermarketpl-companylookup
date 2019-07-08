e85d2790c7c6d806618f1266c1da0bcdc07efd94
Paweł Sośnicki <sosnicki.pawel@gmail.com>
2019-07-08 18:51:07 +0200

    Obsluga API GUS

--- index.php
+++ index.php
@@ -6,5 +6,8 @@ use Aftermarketpl\CompanyLookup\Env;
 $vies = new Aftermarketpl\CompanyLookup\ViesReader();
 $vat =  new Aftermarketpl\CompanyLookup\VatReader();
 $ceidg = new Aftermarketpl\CompanyLookup\CeidgReader(Env::$ceidgapikey);
+$gus = new Aftermarketpl\CompanyLookup\GusReader(Env::$gusapikey);
 
-print_r($vat->lookup('5342532004'));
+print_r($gus->lookup('5342532004'));
+print_r($gus->lookupRegon('365810667'));
+print_r($gus->lookupKrs('0000645598'));
---
+++ src/GusReader.php
@@ -0,0 +1,160 @@
+<?php 
+
+namespace Aftermarketpl\CompanyLookup;
+
+use Throwable;
+use Aftermarketpl\CompanyLookup\Exceptions\GusReaderException;
+use SoapClient;
+
+use GusApi\BulkReportTypes;
+use GusApi\Exception\InvalidUserKeyException;
+use GusApi\Exception\NotFoundException;
+use GusApi\GusApi;
+use GusApi\ReportTypes;
+use DateTimeImmutable;
+use GusApi\SearchReport;
+
+/**
+ * https://api.stat.gov.pl/Home/RegonApi/
+ */
+class GusReader
+{
+    use Traits\ResolvesVatid;
+    use Traits\ValidatesVatid;
+
+    /**
+     * API key
+     */
+    private $apikey = '';
+    private $api = null;
+
+    private $options = [];
+
+    /**
+     * 
+     */
+    public function __construct(string $apikey = '')
+    {
+        $this->apikey = $apikey;
+
+        try {
+            $this->api = new GusApi($apikey);
+            $this->api->login();
+        } catch(\Throwable $e) {
+            throw new CeidgReaderException('Checking status currently not available');
+        }           
+        
+    }
+
+    /**
+     * Lookup company by vatid
+     */
+    public function lookup(string $vatid)
+    {
+        $vatid = $this->validateVatid($vatid, 'PL');
+        list($country, $number) = $this->resolveVatid($vatid);
+
+        try {
+            $gusReports = $this->api->getByNip($number);
+
+            foreach ($gusReports as $gusReport) {
+                if($gusReport->getActivityEndDate())
+                    continue; // ommit inactive
+                return $this->mapCompanyData($gusReport);
+            }
+
+        } catch (InvalidUserKeyException $e) {
+            throw new GusReaderException('Checking status currently not available [Invalid Api key]');
+        
+        } catch (NotFoundException $e) {
+            return [
+                'result' => 'invalid',
+                'vatid' => $vatid
+            ];            
+        }
+    }
+
+    /**
+     * Lookup company by KRS
+     */
+    public function lookupKRS(string $krs)
+    {
+        try {
+            $gusReports = $this->api->getByKrs($krs);
+
+            foreach ($gusReports as $gusReport) {
+                if($gusReport->getActivityEndDate())
+                    continue; // ommit inactive
+                return $this->mapCompanyData($gusReport);
+            }
+
+        } catch (InvalidUserKeyException $e) {
+            throw new GusReaderException('Checking status currently not available [Invalid Api key]');
+        
+        } catch (NotFoundException $e) {
+            return [
+                'result' => 'invalid',
+                'krs' => $krs
+            ];            
+        }
+    }
+
+    /**
+     * Lookup company by REGON
+     */
+    public function lookupREGON(string $regon)
+    {
+        try {
+            $gusReports = $this->api->getByRegon($regon);
+
+            foreach ($gusReports as $gusReport) {
+                if($gusReport->getActivityEndDate())
+                    continue; // ommit inactive
+                return $this->mapCompanyData($gusReport);
+            }
+
+        } catch (InvalidUserKeyException $e) {
+            throw new GusReaderException('Checking status currently not available [Invalid Api key]');
+        
+        } catch (NotFoundException $e) {
+            return [
+                'result' => 'invalid',
+                'regon' => $regon
+            ];            
+        }
+    }
+
+
+    /**
+     * 
+     */
+    protected function mapCompanyData(SearchReport $gusReport) {
+        return [
+            'result' => 'valid',
+            'country' => 'PL',
+            'vatid' => $gusReport->getNip(),
+            'regon' => $gusReport->getRegon(),
+            'company' => (string) $gusReport->getName(),
+            'address' => (string) $gusReport->getStreet().' '.$gusReport->getPropertyNumber() . ( $gusReport->getApartmentNumber() ? '/'.$gusReport->getApartmentNumber() : ''),
+            'zip' => (string) $gusReport->getZipCode(),
+            'city' => (string) $gusReport->getCity(),
+        ]; 
+    }
+
+    /**
+     * 
+     */
+    protected function handleFullReport(SearchReport $report) {
+        switch($report->getType())
+        {
+            case 'p': // osoba prawna
+                $reportType = ReportTypes::REPORT_PUBLIC_LAW;
+                break;
+
+            case 'f': // osoba fizyczna
+                $reportType = ReportTypes::REPORT_ACTIVITY_PHYSIC_PERSON;
+                break;                    
+        }
+        return $this->api->getFullReport($report, $reportType);
+    }
+}
